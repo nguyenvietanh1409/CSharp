c3f7fdc8b4ad9f496e5e6090be859c92cd7c1b39
nguyenvietanh1409 <vietanh20030914@gmail.com>
2022-08-04 14:14:55 +0700

    Lab8

---
+++ Lab8/Lab8-4/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        list.Add(10);
+        list.Add(1);
+        list.Add(1230);
+        Console.WriteLine("{0}", GetFirst(list));
+    }
+
+    private static int? GetFirst(List<int> list)
+    {
+        if (list == null || list.Count==0)
+        {
+            return null;
+        }
+        return list[0];
+    }
+}
---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Product
+{
+    private string name;
+    private double cost;
+    private int onhand;
+    public Product(string name, double cost, int onhand)
+    {
+        this.name = name;
+        this.cost = cost;
+        this.onhand = onhand;
+    }
+    public override string ToString()
+    {
+        return String.Format("{0,-10}Cost: {1,6:C} On hand: {2}", name, cost, onhand);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Product> inv = new List<Product>();
+        inv.Add(new Product("A", 5.9, 3));
+        inv.Add(new Product("B", 8.2, 2));
+        inv.Add(new Product("C", 3.5, 4));
+        inv.Add(new Product("D", 1.8, 8));
+        Console.WriteLine("Product List: ");
+        foreach (Product i in inv) {
+            Console.WriteLine(" " + i);
+        }
+    }
+}
